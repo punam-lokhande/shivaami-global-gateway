1adcf0ffe2484ccfea7703880d7dee278b80f4ae
punam-lokhande <punam@shivaami.com>
2026-02-23 18:03:13 +0530

    Changes in Zoho Campaign Newsletter

--- src/pages/Newsletter.tsx
+++ src/pages/Newsletter.tsx
@@ -78,11 +78,15 @@ export default function Newsletter() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const zohoEmailInput = document.getElementById('EMBED_FORM_EMAIL_LABEL') as HTMLInputElement;
-    const zohoSubmitButton = document.getElementById('zcWebOptin') as HTMLInputElement;
+    // const zohoSubmitButton = document.getElementById('zcWebOptin') as HTMLInputElement;
 
-    if (zohoEmailInput && zohoSubmitButton) {
+     const zohoForm = document.getElementById('zcampaignOptinForm') as HTMLFormElement;
+   
+    if (zohoEmailInput && zohoForm) {
+      
       zohoEmailInput.value = email;
-      zohoSubmitButton.click();
+      
+      zohoForm.submit();
     } else {
       console.error('Zoho form elements not found.');
     }
